54b15537bbbc76b8922823189ff7e0bedc9312fa
Willian Caires <willianaika12@gmail.com>
2022-12-03 01:25:03 -0300

    Variaveis e Tipos
    Primeiro módulo do curso de JavaScript: tipos, variaveis e funções.

---
+++ JavaScript - tipos, variaveis e funcoes/tiposEVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+const inteiro = 2;
+const flutuante = 3.5;
+const string = "Mundo";
+const stringNumero = "2";
+const boolean = true;
+
+console.log(inteiro * flutuante);
+console.log(inteiro * string);
+console.log(inteiro * stringNumero);
+console.log(inteiro + string);
+console.log(inteiro + stringNumero);
+console.log(boolean);
+console.log(inteiro < stringNumero);
+console.log(inteiro == stringNumero);
+console.log(inteiro === stringNumero); 
